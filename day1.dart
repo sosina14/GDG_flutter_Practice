3efc137a40b00cd2b5264fe8f09fab851e5ea436
sosina14 <sosiayu14@gmail.com>
2025-11-26 15:29:14 +0300

    first class of flutter at GDG

---
+++ day1.dart
@@ -0,0 +1,20 @@
+// void main() {
+//   print("Hello Dart!");
+//   var x = 10;
+//   var y = 20;
+//   print(x+y);
+//   for (int i = 1; i<=5 ; i++){
+//     for (int j = 0; j <= i; j++){
+//       print(j);
+//     }
+//     // print(i ; i);
+//     // print('*');
+//   }
+// }
+
+int add(int x, int y){ //function declaration
+  return x + y;
+}
+void main(){
+  print(add(5,3)); // calling function
+}
