38ac0a22beda0bdba2335c86893ece71781b715f
sosina14 <sosiayu14@gmail.com>
2025-12-10 13:29:37 +0300

    update

---
+++ Task_1.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+void main(){
+// Tasks 1,write a program that prints hello world 
+  print("Hello World");
+
+// Task 2. write a program that checks if a number is divisible by 3 print
+// ‘fizz’ , ir a number is divisible by 5 print ‘buzz’ if a number is
+// divisible by both 3 and 5 print ‘ fizzbuzz’.
+  int num = 10;
+  if (num % 3 == 0 && num % 5 == 0);
+    print("fizzbuzz");
+  if(num % 5 == 0);
+    print("buzz");
+  if(num % 3 == 0);
+    print("fizz");
+  
+
+  for(int i = 0 ; i <= 5 ; i++){
+    for(int j = 0 ; j <= i; ++j){
+      stdout.write('*');
+    }
+    print('');
+  }
+  for(int i = 0 ; i <= 20 ; i++){
+    if (i % 2 == 0);
+      print(i);
+  }
+}
+
+
+
+
+
+// 3. Create a function that takes user name and age as
+// parameters. print user information.
